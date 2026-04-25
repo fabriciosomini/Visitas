59b8592853bab56ab325bdda322dd0fe4bf2ab87
Fabricio Somini <fabricio.somini@gmail.com>
2026-04-24 22:46:34 -0300

    Update PreviewFoldable device spec to landscape orientation

--- app/src/main/java/com/msmobile/visitas/ui/theme/PreviewAnnotations.kt
+++ app/src/main/java/com/msmobile/visitas/ui/theme/PreviewAnnotations.kt
@@ -2,6 +2,6 @@ package com.msmobile.visitas.ui.theme
 
 import androidx.compose.ui.tooling.preview.Preview
 
-@Preview(name = "Foldable", device = "spec:width=673dp,height=841dp")
+@Preview(name = "Foldable", device = "spec:width=673dp,height=841dp,orientation=landscape")
 annotation class PreviewFoldable
 
