b6ba9047cc8fc57960e4106b551fbc0c574e21f7
Fabricio Somini <fabricio.somini@gmail.com>
2026-04-24 22:41:48 -0300

    Add PreviewFoldable annotation for foldable device previews

---
+++ app/src/main/java/com/msmobile/visitas/ui/theme/PreviewAnnotations.kt
@@ -0,0 +1,7 @@
+package com.msmobile.visitas.ui.theme
+
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(name = "Foldable", device = "spec:width=673dp,height=841dp")
+annotation class PreviewFoldable
+
